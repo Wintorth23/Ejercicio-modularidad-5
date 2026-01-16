7e73b7cf44654eb97322f2eb384970bd07745743
Jeferson_Vasconez <Jhvasconez@espe.edu.ec>
2026-01-16 10:35:04 -0500

    v 1.0

---
+++ Documents/Proyectos net/Ejemplo5 MOD/src/ejemplo5/mod/IMostrar.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ejemplo5.mod;
+
+/**
+ *
+ * @author jepeh
+ */
+public interface IMostrar {
+   void mostrarRegistro(Estudiante e, double promediofinal, double promedioInical,boolean aprovado1,boolean aprovado2 );
+   
+}
